455b844ada1d53c75081e0518a496277681ef5f1
Gagan Mittal <v-gamitt@microsoft.com>
2018-05-14 20:24:28 -0700

    Added automation for 1 SVT of ContainerInstances

--- src/AzSK/Framework/Core/SVT/Services/ContainerInstances.ps1
+++ src/AzSK/Framework/Core/SVT/Services/ContainerInstances.ps1
@@ -6,21 +6,24 @@ class ContainerInstances: SVTBase
     ContainerInstances([string] $subscriptionId, [string] $resourceGroupName, [string] $resourceName): 
         Base($subscriptionId, $resourceGroupName, $resourceName) 
     { 
-        #$this.GetResourceObject();
+        $this.GetResourceObject();
     }
 
     ContainerInstances([string] $subscriptionId, [SVTResource] $svtResource): 
         Base($subscriptionId, $svtResource) 
     { 
-        #$this.GetResourceObject();
+        $this.GetResourceObject();
     }
 
     hidden [PSObject] GetResourceObject()
     {
         if (-not $this.ResourceObject) 
 		{
-            $this.ResourceObject = Get-AzureRmContainerGroup -Name $this.ResourceContext.ResourceName `
-											-ResourceGroupName $this.ResourceContext.ResourceGroupName -ErrorAction SilentlyContinue
+           # $this.ResourceObject = Get-AzureRmContainerGroup -Name $this.ResourceContext.ResourceName `
+											#-ResourceGroupName $this.ResourceContext.ResourceGroupName -ErrorAction SilentlyContinue
+
+            $this.ResourceObject = Find-AzureRmResource -ResourceNameEquals $this.ResourceContext.ResourceName `
+											-ResourceGroupNameEquals $this.ResourceContext.ResourceGroupName -ExpandProperties
 
             if(-not $this.ResourceObject)
             {
@@ -29,4 +32,24 @@ class ContainerInstances: SVTBase
         }
         return $this.ResourceObject;
     }
+
+	hidden [ControlResult] CheckPublicIPAndPorts([ControlResult] $controlResult)
+    {
+		if([Helpers]::CheckMember($this.ResourceObject, "properties.ipAddress"))
+		{
+			$controlResult.VerificationResult = [VerificationResult]::Verify; 
+			$controlResult.SetStateData("Public IP address and ports assigned to the container", $this.ResourceObject.properties.ipAddress);
+			$controlResult.AddMessage([MessageData]::new("Review following public IP address and ports assignment to the container - ["+ $this.ResourceContext.ResourceName +"]",
+								$this.ResourceObject.properties.ipAddress));
+		}
+		else
+		{
+			$controlResult.AddMessage([VerificationResult]::Passed, 
+                            [MessageData]::new("No public IP address has been assigned to the container - ["+ $this.ResourceContext.ResourceName +"]"));
+		}
+  
+        return $controlResult;
+    }
+
+	
 }
