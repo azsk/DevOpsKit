88de18d9931a1c46ad808f3eafe0284e09939e0a
v-nitja <v-nitja@microsoft.com>
2018-07-10 15:11:19 +0530

    changes for action group name

--- src/AzSK/Framework/Core/SubscriptionSecurity/Alerts.ps1
+++ src/AzSK/Framework/Core/SubscriptionSecurity/Alerts.ps1
@@ -672,7 +672,7 @@ class Alerts: CommandBase
 			$actionGroupArmResource = $actionGroupArm.resources | Where-Object { $_.Name -eq $([Constants]::AlertActionGroupName) } 
 			$actionGroupArmResourceOutput = $actionGroupArm.outputs.actionGroupId
 			$actionGroupArmResource.name="ResourceDeploymentActionGroup"
-			$actionGroupArmResourceOutput.value = $actionGroupArmResourceOutput.value.Replace($([Constants]::AlertActionGroupName),$([Constants]::ResourceCreationActionGroupName));
+			$actionGroupArmResourceOutput.value = $actionGroupArmResourceOutput.value.Replace($([Constants]::AlertActionGroupName),$([Constants]::ResourceDeploymentActionGroupName));
 			$actionGroupArmResource.properties.PSObject.Properties.Remove('emailReceivers')
             $actionGroupArmResource.properties.PSObject.Properties.Remove('smsReceivers')
 			
@@ -776,7 +776,7 @@ class Alerts: CommandBase
 		}
 		elseif($type -eq "ResourceCreation")
 		{
-			$ActionGroup = [Constants]::ResourceCreationActionGroupName
+			$ActionGroup = [Constants]::ResourceDeploymentActionGroupName
 		}
 
 		$actionGroupResource = $this.GetAlertActionGroup($this.ResourceGroup, $ActionGroup);
@@ -813,7 +813,7 @@ class Alerts: CommandBase
 		}
 		elseif($type -eq "ResourceCreation")
 		{
-			$ActionGroup = [Constants]::ResourceCreationActionGroupName
+			$ActionGroup = [Constants]::ResourceDeploymentActionGroupName
 			$Alertname = "WebHookForResourceCreationAlerts"
 		}
 		else
