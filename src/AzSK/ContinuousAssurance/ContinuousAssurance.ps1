e054fdc503637847c400b8832931c12034415b1d
unknown <v-shbham@Microsoft.com>
2018-07-11 20:47:56 +0530

    revert back oms null var check

--- src/AzSK/ContinuousAssurance/ContinuousAssurance.ps1
+++ src/AzSK/ContinuousAssurance/ContinuousAssurance.ps1
@@ -341,6 +341,7 @@ function Update-AzSKContinuousAssurance
 		
 		[Parameter(Mandatory = $false, ParameterSetName = "Default")]
 		[Parameter(Mandatory = $false, ParameterSetName = "CentralScanMode")]
+		[ValidateNotNullOrEmpty()]	
         [string]
 		[Alias("owid")]
 		$OMSWorkspaceId,
