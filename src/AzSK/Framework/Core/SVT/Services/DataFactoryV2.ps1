286a3cbd1f25efdde8bcc877e85510b0824b9a7b
Sudhindranath Byna <sbyna@microsoft.com>
2018-07-16 15:43:39 +0530

    fixes related to ADF v2 controls

--- src/AzSK/Framework/Core/SVT/Services/DataFactoryV2.ps1
+++ src/AzSK/Framework/Core/SVT/Services/DataFactoryV2.ps1
@@ -3,29 +3,13 @@ Set-StrictMode -Version Latest
 class DataFactoryV2: SVTBase
 {       
 
-    hidden [PSObject] $ResourceObject;
-
-    DataFactory([string] $subscriptionId, [string] $resourceGroupName, [string] $resourceName): 
+    DataFactoryV2([string] $subscriptionId, [string] $resourceGroupName, [string] $resourceName): 
         Base($subscriptionId, $resourceGroupName, $resourceName) 
     { 
-		$this.GetResourceObject();
     }
 
-	DataFactory([string] $subscriptionId, [SVTResource] $svtResource): 
+	DataFactoryV2([string] $subscriptionId, [SVTResource] $svtResource): 
         Base($subscriptionId, $svtResource) 
     { 
-		 $this.GetResourceObject();
     }
-
-	hidden [PSObject] GetResourceObject()
-    {
-        if (-not $this.ResourceObject) {
-            $this.ResourceObject = Get-AzureRmDataFactoryV2 -Name $this.ResourceContext.ResourceName -ResourceGroupName $this.ResourceContext.ResourceGroupName
-			if(-not $this.ResourceObject)
-			{
-				throw ([SuppressedException]::new(("Resource '{0}' not found under Resource Group '{1}'" -f ($this.ResourceContext.ResourceName), ($this.ResourceContext.ResourceGroupName)), [SuppressedExceptionType]::InvalidOperation))
-			}
-		}
-	}
-	return $this.ResourceObject;
 }
