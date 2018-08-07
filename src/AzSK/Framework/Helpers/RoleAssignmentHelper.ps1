5bd739dafcd35565fbc1f1bc5660d52ee3e48b85
Shruti Bhambhani (Tata Consultancy Services Ltd) <v-shbham@microsoft.com>
2018-08-07 11:27:12 +0530

    Resolved review comments

--- src/AzSK/Framework/Helpers/RoleAssignmentHelper.ps1
+++ src/AzSK/Framework/Helpers/RoleAssignmentHelper.ps1
@@ -149,13 +149,13 @@ class RoleAssignmentHelper
 			#get role definition details only for unique roles
             $webResponse.properties | Select-Object roleDefinitionId -Unique | ForEach-Object{
 			    $roleDefinitionId = $_.roleDefinitionId.Substring($_.roleDefinitionId.LastIndexOf("/") + 1);
-			    $roleDefinitionName = "";
+			    $roleDefinitionName = [string]::Empty()
 			
 			    $roleDefinition = (Get-AzureRmRoleDefinition -Id $roleDefinitionId -ErrorAction SilentlyContinue) | Select-Object -First 1
-			    if($roleDefinition) 
+			    if($roleDefinition -and [Helpers]::CheckMember($roleDefinition,"Name")) 
 			    { 
 				    $roleDefinitionName = $roleDefinition.Name;
-			    };
+			    }
 			    $roleDefnMapping.Add($roleDefinitionId,$roleDefinitionName) 
 			}
 			#assign role name(roleDefinitionName) to each role assignment 
