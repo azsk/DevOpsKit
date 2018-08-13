bb15ea8b49ddec502fd24fd2206dbce379850b24
Zhilmil Gupta (Tata Consultancy Services) <v-zhgup@microsoft.com>
2018-08-13 13:24:19 +0530

    minor change

--- src/AzSK/Framework/Core/SVT/Services/KeyVault.ps1
+++ src/AzSK/Framework/Core/SVT/Services/KeyVault.ps1
@@ -64,7 +64,7 @@ class KeyVault: SVTBase
 				}
 			}catch
 			{
-				$this.HasFetchSecretsPermissions=$false;
+				$this.HasFetchKeysPermissions=$false;
 				$this.HasFetchSecretsPermissions=$false;
 			}
 	}
