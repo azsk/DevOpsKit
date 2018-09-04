d887de2082c6b21a9f8fbba0d568052541eb2e41
Mayur Kularni <mayurkulkarni012@gmail.com>
2018-09-04 12:28:45 +0530

    Added validation decorator

--- src/PythonSrc/WebJob/webjob.py
+++ src/PythonSrc/WebJob/webjob.py
@@ -5,6 +5,18 @@ from msrestazure.azure_active_directory import MSIAuthentication
 from azure.keyvault import KeyVaultClient
 from azure.storage.file import FileService
 
+def validate(caller, *args, **kwargs):
+	"""Decorator to validate calling function"""
+	def executor():
+		result = None
+		try
+			result = caller(*args, **kwargs)
+		except Exception as e:
+			print("Error at {} Exception: {}".format(caller.__name__, e))
+		return result
+	return executor
+
+@validate
 def get_server_credentials():
 	credentials = MSIAuthentication(resource='https://vault.azure.net')
 	key_vault_client = KeyVaultClient(credentials)
@@ -19,15 +31,18 @@ def get_server_credentials():
 	print("Secret received is: {}".format(secret))
 	return secret.value
 
+@validate
 def get_csv_from_mysql():
     cnxn = pyodbc.connect(get_server_credentials())
     cursor = cnxn.cursor()
+    # todo: remove TOP(1000)
     query = "SELECT TOP(1000) * FROM DBO.LASTKNOWNSERVICESCANRESULTJOINED"
     df = pd.read_sql_query(query, cnxn)
     df.set_index("Id", inplace=True)
     df.to_csv("data_from_server.csv")
     print("Saved to disk")
 
+@validate
 def save_to_storage():
 	#todo: save account key in vault
     fs = FileService(account_name="recoenginestorage",
