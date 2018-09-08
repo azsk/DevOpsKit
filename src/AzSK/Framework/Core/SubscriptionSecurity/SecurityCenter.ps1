33533a6182cd2984b7a26e7473871f2ad6315ce7
Sudhindranath Byna <sbyna@microsoft.com>
2018-09-08 23:07:06 +0530

    Fixed the api in scan

--- src/AzSK/Framework/Core/SubscriptionSecurity/SecurityCenter.ps1
+++ src/AzSK/Framework/Core/SubscriptionSecurity/SecurityCenter.ps1
@@ -36,6 +36,29 @@ class SecurityCenter: AzSKRoot
 		$this.LoadCurrentPolicy();
 	}
 
+
+	hidden [string[]] CheckASCCompliance()
+	{
+		$statuses = @();
+		$response = $this.CheckAutoProvisioningSettings();
+		if(-not [string]::IsNullOrWhiteSpace($response))
+		{
+			$statuses += $response;
+		}
+		$response = $this.CheckSecurityContactSettings();
+		if(-not [string]::IsNullOrWhiteSpace($response))
+		{
+			$statuses += $response;
+		}
+
+		$response = $this.CheckSecurityPolicySettings();
+		if(($response | Measure-Object).Count -gt 0)
+		{
+			$statuses += $response;
+		}
+		return $statuses;
+	}
+
 	hidden [void] LoadCurrentPolicy()
 	{
 		if($null -ne $this.PolicyObject -and $null -ne $this.PolicyObject.policySettings)
@@ -87,6 +110,20 @@ class SecurityCenter: AzSKRoot
 		return $messages;
 	}
 
+	[string] CheckAutoProvisioningSettings()
+	{		
+		if($null -ne $this.PolicyObject -and $null -ne $this.PolicyObject.autoProvisioning)
+		{			
+			$autoProvisioningUri = [WebRequestHelper]::AzureManagementUri + "subscriptions/$($this.SubscriptionContext.SubscriptionId)/providers/$([SecurityCenterHelper]::ProviderNamespace)/$([SecurityCenterHelper]::AutoProvisioningSettingsApi)/default$([SecurityCenterHelper]::ApiVersionNew)";
+			$response = [WebRequestHelper]::InvokeGetWebRequest($autoProvisioningUri);
+			if(-not ([Helpers]::CheckMember($response,"properties.autoProvision") -and $response.properties.autoProvision -eq "On" ))
+			{
+				return "AutoProvisioning: [Failed]"
+			}
+		}
+		return $null;
+	}
+
 	[MessageData[]] SetSecurityContactSettings()
 	{
 		[MessageData[]] $messages = @();
@@ -98,6 +135,29 @@ class SecurityCenter: AzSKRoot
 		  	[WebRequestHelper]::InvokeWebRequest([Microsoft.PowerShell.Commands.WebRequestMethod]::Put, $securityContactsUri, $body);
 		}
 		return $messages;
+	}
+
+	[string] CheckSecurityContactSettings()
+	{
+		if($null -ne $this.PolicyObject -and $null -ne $this.PolicyObject.securityContacts)
+		{
+			$securityContactsUri = [WebRequestHelper]::AzureManagementUri + "subscriptions/$($this.SubscriptionContext.SubscriptionId)/providers/$([SecurityCenterHelper]::ProviderNamespace)/$([SecurityCenterHelper]::SecurityContactsApi)/default1$([SecurityCenterHelper]::ApiVersionNew)";
+			$response = [WebRequestHelper]::InvokeGetWebRequest($securityContactsUri);
+			if([Helpers]::CheckMember($response,"properties.email") -and -not [string]::IsNullOrWhiteSpace($response.properties.email) `
+				-and [Helpers]::CheckMember($response,"properties.phone") -and -not [string]::IsNullOrWhiteSpace($response.properties.phone))				
+			{
+				$this.ContactEmail = $response.properties.email;
+				$this.ContactPhoneNumber = $response.properties.phone;
+				if(-not ([Helpers]::CheckMember($response,"properties.email") -and -not [string]::IsNullOrWhiteSpace($response.properties.email) `
+				-and [Helpers]::CheckMember($response,"properties.phone") -and -not [string]::IsNullOrWhiteSpace($response.properties.phone) `
+				-and [Helpers]::CheckMember($response,"properties.alertNotifications") -and $response.properties.alertNotifications -eq "On" `
+				-and [Helpers]::CheckMember($response,"properties.alertsToAdmins") -and $response.properties.alertsToAdmins -eq "On"))			
+				{
+					return "SecurityContactsConfig: [Failed]"
+				}				
+			}
+		}
+		return $null;
 	}
 
 	[MessageData[]] SetSecurityPolicySettings()
@@ -114,6 +174,15 @@ class SecurityCenter: AzSKRoot
 		return $messages;
 	}
 
+	[string[]] CheckSecurityPolicySettings()
+	{
+		if($null -ne $this.PolicyObject -and $null -ne $this.PolicyObject.policySettings)
+		{	
+			return $this.ValidatePolicyObject();											
+		}
+		return $null;
+	}
+
 	[void] UpdatePolicyObject()
 	{
 		if($null -ne $this.CurrentPolicyObject -and $null -ne $this.PolicyObject.policySettings)	
@@ -127,5 +196,24 @@ class SecurityCenter: AzSKRoot
 			}
 			$this.PolicyObject.policySettings.properties.parameters = $currentPolicyObj;
 		}		
+	}	
+
+	[string[]] ValidatePolicyObject()
+	{
+		[string[]] $MisConfiguredPolicies = @();
+		if($null -ne $this.CurrentPolicyObject -and $null -ne $this.PolicyObject.policySettings)	
+		{
+			$currentPolicyObj = $this.CurrentPolicyObject.Properties.parameters;
+			$defaultPoliciesNames = Get-Member -InputObject $this.PolicyObject.policySettings.properties.parameters -MemberType NoteProperty | Select-Object Name
+			$configuredPolicyObject = $this.PolicyObject.policySettings.properties.parameters;
+			$defaultPoliciesNames | ForEach-Object {
+				$policyName = $_.Name;				
+				if($currentPolicyObj.$policyName.value -ne $configuredPolicyObject.$policyName.value)
+				{
+					$MisConfiguredPolicies += ("Misconfigured Policy: [" + $policyName + "]");
+				}
+			}
+		}
+		return $MisConfiguredPolicies;		
 	}	
 }
