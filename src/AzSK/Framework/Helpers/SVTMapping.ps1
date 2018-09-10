81f617ff9543129473ee86e4a4acc86f31eb366c
Sapan Shah <v-sapsh@microsoft.com>
2018-09-10 22:17:31 +0530

    - Added fixscript for Container Registry adminUserEnable control - Update check AdminUserEnable logic of ContainerRegistry for error fix - Update CheckAAD control of AppService for error fix

--- src/AzSK/Framework/Helpers/SVTMapping.ps1
+++ src/AzSK/Framework/Helpers/SVTMapping.ps1
@@ -243,6 +243,8 @@ class SVTMapping
 			ClassName = "ContainerRegistry";
 			JsonFileName = "ContainerRegistry.json";
 			ResourceTypeName = "ContainerRegistry";
+			FixClassName = "ContainerRegistryFix";
+			FixFileName = "ContainerRegistryFix.ps1";
         }
     );
 
