b21c5a69415b048a52520e4452cd1fd6053f2d6b
Sudhindranath Byna <sbyna@microsoft.com>
2018-09-14 10:00:34 +0530

    minor fixes

--- src/AzSK/Framework/Core/AzSKInfo/SecurityRecommendationsReport.ps1
+++ src/AzSK/Framework/Core/AzSKInfo/SecurityRecommendationsReport.ps1
@@ -29,43 +29,27 @@ class SecurityRecommendationsReport: CommandBase
 	}
 
 	hidden [System.Object]get_hash([SecurityReportInput] $Input){
-
 		$hash_val = 1;
-
-        if(-not [string]::IsNullOrWhiteSpace($Input.Categories))
+        if(($Input.Categories| Measure-Object).Count -gt 0)
 	    {
-
-		foreach ($category in $Input.Categories)
-		{
-			[PSObject] $f=$category; 
-			
-			$hash_val = $hash_val * $this.category_hash.$f;
-
-			$hash_val = $hash_val % 824633720831 ;
-			
-		}
-		
-	   }
-
-	   if(-not [string]::IsNullOrWhiteSpace($Input.Features))
-	   {
-
-		foreach ($feature in $Input.Features)
-		{
-			[PSObject] $p=$feature;
-
-			$categories = $this.get_categories.$p
-
-			[PSObject] $f=$categories[0];
-
-			$hash_val = $hash_val * $this.category_hash.$f;
-
-			$hash_val = $hash_val % 824633720831 ;
-			
-		}
-		
-	   }
-
+			foreach ($category in $Input.Categories)
+			{
+				[PSObject] $f=$category; 
+				$hash_val = $hash_val * $this.category_hash.$f;
+				$hash_val = $hash_val % 824633720831 ;
+			}
+	    }
+	    if(($Input.Features | Measure-Object).Count -gt 0)
+	    {
+			foreach ($feature in $Input.Features)
+			{
+				[PSObject] $p=$feature;
+				$categories = $this.get_categories.$p
+				[PSObject] $f=$categories[0];
+				$hash_val = $hash_val * $this.category_hash.$f;
+				$hash_val = $hash_val % 824633720831 ;
+			}
+	    }
 		return $hash_val;
 	}
 
@@ -96,71 +80,38 @@ class SecurityRecommendationsReport: CommandBase
 	[MessageData[]] GenerateReport([string] $ResourceGroupName, [ResourceTypeName[]] $ResourceTypeNames,[string[]] $Categories)
 
     {		    	    
-
 		[MessageData[]] $messages = @();	
-		
 		$this.get_categories = [ConfigurationHelper]::LoadOfflineConfigFile("get_categories.json", $false);
-
 		#$this.get_categories = $this.get_categories | ConvertFrom-Json;
-
 		$this.category_hash = [ConfigurationHelper]::LoadOfflineConfigFile("category_hash.json", $false);
-
 		#$this.category_hash = $this.category_hash | ConvertFrom-Json;
 		try
-
 		{
-
 			[RecommendedSecurityReport] $report = [RecommendedSecurityReport]::new();
-
 			[SecurityReportInput] $userInput = [SecurityReportInput]::new();
-
 			if(-not [string]::IsNullOrWhiteSpace($ResourceGroupName))
-
 			{
-
 				$resources = Get-AzureRmResource -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue
-
 				if(($resources | Measure-Object).Count -gt 0)
-
 				{
-
 					[SVTMapping]::GetSupportedResourceMap();
-
 					$resources | ForEach-Object{						
-
 						if($null -ne [SVTMapping]::SupportedResourceMap[$_.ResourceType.ToLower()])
-
 						{
-
 							$userInput.Features += ([SVTMapping]::SupportedResourceMap[$_.ResourceType.ToLower()]).ToString();
-
 						}
-
 					}	
-
 					$report.ResourceGroupName = $ResourceGroupName;
-
-				}				
-
-				elseif(($ResourceTypeNames | Measure-Object).Count -gt 0)
-
-				{
-
-					$ResourceTypeNames | ForEach-Object { $userInput.Features += $_.ToString();}
-
 				}
-
-				elseif(($Categories | Measure-Object).Count -gt 0)
-
-				{
-
-					$userInput.Categories = Categories
-
-				}
-
-			}			
-
-
+			}				
+			elseif(($ResourceTypeNames | Measure-Object).Count -gt 0)
+			{
+				$ResourceTypeNames | ForEach-Object { $userInput.Features += $_.ToString();}
+			}
+			elseif(($Categories | Measure-Object).Count -gt 0)
+			{
+				$userInput.Categories = $Categories
+			}
 
 			#$uri = "http://104.211.204.4/recommend";
 
