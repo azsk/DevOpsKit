c3e0ba54028ab31f2190281b850c07e02d1aa619
Rini Chandra (Tata Consultancy Services Ltd) <v-rincha@microsoft.com>
2019-03-08 17:44:51 +0530

    bug fix and control fix

--- src/AzSK/Framework/Core/SVT/Services/NotificationHub.ps1
+++ src/AzSK/Framework/Core/SVT/Services/NotificationHub.ps1
@@ -44,13 +44,15 @@ class NotificationHub: SVTBase
                                                 | Select-Object -Property Name, Rights  
         if((($accessPolicieswithManageRights | Measure-Object).Count -eq 1) -and ($accessPolicieswithManageRights.Name -eq "DefaultFullSharedAccessSignature")) {
             $controlResult.AddMessage([VerificationResult]::Verify,
-                            [MessageData]::new("Only the default authorization rule has 'Manage' security claim access rights for resource -  ["+ $this.ResourceContext.ResourceName +"]"  , 
+                            [MessageData]::new("Only the default authorization rule has 'Manage' security claim access rights for resource -  ["+ $this.ResourceContext.ResourceName +"]. Please ensure that these authorization rules are not used at the client end."  , 
                             $accessPolicieswithManageRights));
+
+            $controlResult.SetStateData("Access policy with 'Manage' rights",$accessPolicieswithManageRights);
         }
         else {
                 if($null -ne $accessPolicieswithManageRights){
-                    $controlResult.AddMessage([VerificationResult]::Failed,
-                                            [MessageData]::new("Authorization rules having 'Manage' security claim access rights for resource -  ["+ $this.ResourceContext.ResourceName +"]"  , 
+                    $controlResult.AddMessage([VerificationResult]::Verify,
+                                            [MessageData]::new("Authorization rules having 'Manage' security claim access rights for resource -  ["+ $this.ResourceContext.ResourceName +"]. Please ensure that these authorization rules are not used at the client end."  , 
                                             $accessPolicieswithManageRights));
             
                     $controlResult.SetStateData("Access policies with 'Manage' rights",$accessPolicieswithManageRights);
