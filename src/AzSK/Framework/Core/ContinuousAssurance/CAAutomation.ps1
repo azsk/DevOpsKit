85014797f966fe4d401317cf08695bd854dcb956
Zhilmil Gupta (Tata Consultancy Services Ltd) <v-zhgup@microsoft.com>
2019-03-28 16:19:33 +0530

    Removed the wrong status for runbook version check

--- src/AzSK/Framework/Core/ContinuousAssurance/CAAutomation.ps1
+++ src/AzSK/Framework/Core/ContinuousAssurance/CAAutomation.ps1
@@ -1417,7 +1417,7 @@ class CCAutomation: CommandBase
 		else
 		{
 			$resultMsg = "CA Runbook is too old.`r`nRun command 'Update-AzSKContinuousAssurance -SubscriptionId <subId>'."
-			$resultStatus = "OK"
+			$resultStatus = "Unhealthy"
 			$shouldReturn = $true
 		}	
 		if($shouldReturn)
