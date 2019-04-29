40522ae687e8717c2a6035f0ccdab326deafb4b3
Zhilmil Gupta (Tata Consultancy Services Ltd) <v-zhgup@microsoft.com>
2019-04-29 12:51:15 +0530

    message change

--- src/AzSK/Framework/Core/PIM/PIMScript.ps1
+++ src/AzSK/Framework/Core/PIM/PIMScript.ps1
@@ -605,7 +605,7 @@ hidden TransitionFromPermanentRolesToPIM()
         {    
             $this.PublishCustomMessage($($permanentRolesForTransition | Format-Table -AutoSize -Wrap UserName, ResourceName, ResourceType, RoleName | Out-String),[MessageType]::Default)
             $this.PublishCustomMessage("");
-            Write-Host "The above shown permanent assignments corresponding PIM roles will assigned. Do you want to continue? (Y/N)" -ForegroundColor Yellow
+            Write-Host "For the above shown permanent assignments corresponding PIM roles will be assigned. Do you want to continue? (Y/N)" -ForegroundColor Yellow
             $ToContinue = Read-Host
             if($ToContinue -eq 'y')
             {            
