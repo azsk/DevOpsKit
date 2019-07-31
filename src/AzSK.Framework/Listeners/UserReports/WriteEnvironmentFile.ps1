11d74f73f7ffc44f3b73b666b77804b9d97ac3f3
Garima Singh <v-garisi@microsoft.com>
2019-07-31 10:07:44 +0530

    Bug Fix 4791727

--- src/AzSK.Framework/Listeners/UserReports/WriteEnvironmentFile.ps1
+++ src/AzSK.Framework/Listeners/UserReports/WriteEnvironmentFile.ps1
@@ -75,6 +75,7 @@ class WriteEnvironmentFile: FileOutputBase
 
 	[void] CommandStartedAction([SubscriptionContext] $context)
 	{
+      
 		$this.SetFilePath($context, [FileOutputBase]::ETCFolderPath, "EnvironmentDetails.LOG");  	
 		$this.AddOutputLog([Constants]::DoubleDashLine);
 
@@ -88,9 +89,9 @@ class WriteEnvironmentFile: FileOutputBase
 		$this.AddOutputLog([Helpers]::ConvertObjectToString($this.InvocationContext.BoundParameters, $false));
 		$this.AddOutputLog([Constants]::DoubleDashLine);
 
-		$loadedModules = (Get-Module | Select-Object -Property Name, Path, Description, Version);
+		$loadedModules = (Get-Module | Select-Object -Property Name, Version, Path | Format-Table | Out-String);
 		$this.AddOutputLog("Loaded PowerShell modules");
-		$this.AddOutputLog([Helpers]::ConvertObjectToString($loadedModules, $false));
+        $this.AddOutputLog([Helpers]::ConvertObjectToString($loadedModules, $false));      
 		$this.AddOutputLog([Constants]::DoubleDashLine);
 
 		$rmContext = [ContextHelper]::GetCurrentRMContext()
