eea06cfd04e2478f616dec2168e9bb6ffdbaecc2
Ritika Singh <v-siriti@microsoft.com>
2019-09-10 13:28:15 +0530

    MySqlControls

--- src/AzSK/Framework/Core/SVT/Services/DBForMySql.ps1
+++ src/AzSK/Framework/Core/SVT/Services/DBForMySql.ps1
@@ -136,7 +136,7 @@ class DBForMySql: AzSVTBase {
                 $controlResult.AddMessage([VerificationResult]::Passed, "Setting 'Allow Access to Azure Services' is disabled.");
             } 
             else {
-              $controlResult.AddMessage([VerificationResult]::Manual, "Enable to verify setting 'Allow Access to Azure Services'.");
+              $controlResult.AddMessage([VerificationResult]::Manual, "Unable to verify setting 'Allow Access to Azure Services'.");
             }   
         }
         return $controlResult
