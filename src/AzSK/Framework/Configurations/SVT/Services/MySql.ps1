e8e66ea7290aba38540dbf93ca2eb53e63fb4495
Ritika Singh <v-siriti@microsoft.com>
2019-09-10 13:15:18 +0530

    Revert "MySqlControls"
    This reverts commit 4f6c784a14c30c5db5f4aa0266651e867f7c53cb.

--- src/AzSK/Framework/Configurations/SVT/Services/MySql.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module 'D:\repository\DevOpsKit1909\svt\src\AzSK\AzSKStaging.psd1'
-
-grs -subscriptionid 'abb5301a-22a4-41f9-9e5f-99badff261f8' -resourcegroupname 'MySqlDemo' -resourcename 'etsmysqlserver' -controlid 'Azure_DBforMySQL_NetSec_Configure_VNet_Rules'
-#Get-AzDiagnosticSetting -ResourceId "subscriptions/abb5301a-22a4-41f9-9e5f-99badff261f8/resourceGroups/MySqlDemo/providers/Microsoft.DBforMySQL/servers/etsmysqlserver" 
-#Get-AzResource -Name 'etsmysqlserver'
