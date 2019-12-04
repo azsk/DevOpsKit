4055a73392ce9a084799399f3a987b5294ed07aa
RohitYadav-msft <v-royad@microsoft.com>
2019-12-04 18:55:03 +0530

    Support for IP range validation

--- src/AzSK.ARMChecker.Lib/StringSingleTokenControlData.cs
+++ src/AzSK.ARMChecker.Lib/StringSingleTokenControlData.cs
@@ -5,5 +5,8 @@
 		public ControlDataMatchType Type { get; set; }
 		public string Value { get; set; }
 		public bool IsCaseSensitive { get; set; }
+		public string IfNoPropertyFound { get; set; }
+		public string startIP { get; set; }
+        public string endIP { get; set; }
 	}
 }
