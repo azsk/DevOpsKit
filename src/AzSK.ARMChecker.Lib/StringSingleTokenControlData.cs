444389eaae3de7d6e6d447824814eddedecc63dc
RohitYadav-msft <53034720+RohitYadav-msft@users.noreply.github.com>
2019-12-06 20:27:53 +0530

    changes in case

--- src/AzSK.ARMChecker.Lib/StringSingleTokenControlData.cs
+++ src/AzSK.ARMChecker.Lib/StringSingleTokenControlData.cs
@@ -6,7 +6,7 @@
 		public string Value { get; set; }
 		public bool IsCaseSensitive { get; set; }
 		public string IfNoPropertyFound { get; set; }
-		public string StartIP { get; set; }
-        public string EndIP { get; set; }
+		public string startIP { get; set; }
+        public string endIP { get; set; }
 	}
 }
