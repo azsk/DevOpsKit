01de7592ae0a8afe74698c6dad7ad891eb72ed33
Garima Singh <v-garisi@microsoft.com>
2019-12-09 18:26:14 +0530

    Bug fix: GSS Error in attestation

--- src/AzSK.Framework/Abstracts/SVTCommandBase.ps1
+++ src/AzSK.Framework/Abstracts/SVTCommandBase.ps1
@@ -99,14 +99,16 @@ class SVTCommandBase: AzCommandBase {
         $startScan = ([CommandBase]$this).InvokeFunction($this.RunAllControls);
         if( ([FeatureFlightingManager]::GetFeatureStatus("EnableScanAfterAttestation","*"))) { 
             if (($this.AttestationOptions.AttestControls -eq "NotAttested") -or ($this.AttestationOptions.AttestControls -eq "All")) {
-                if ($Global:AttestationValue) {
+                if (Get-Variable AttestationValue -Scope Global){
+                    if ($Global:AttestationValue) {
 
-                    $this.PublishCustomMessage(([Constants]::DoubleDashLine))
-                    $this.PublishCustomMessage(([Constants]::HashLine))
-                    $this.PublishCustomMessage(([Constants]::AttestedControlsScanMsg))
-                    $this.PublishCustomMessage(([Constants]::DoubleDashLine))
+                        $this.PublishCustomMessage(([Constants]::DoubleDashLine))
+                        $this.PublishCustomMessage(([Constants]::HashLine))
+                        $this.PublishCustomMessage(([Constants]::AttestedControlsScanMsg))
+                        $this.PublishCustomMessage(([Constants]::DoubleDashLine))
 
-                    ([CommandBase]$this).InvokeFunction($this.ScanAttestedControls,$null);
+                        ([CommandBase]$this).InvokeFunction($this.ScanAttestedControls,$null);
+                    }
                 }
             }
         }
