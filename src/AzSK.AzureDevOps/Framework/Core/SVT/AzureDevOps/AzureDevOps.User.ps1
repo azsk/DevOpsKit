64e368de92411915a138059d694279f874aaf122
v-arbagh@microsoft.com <v-arbagh@microsoft.com>
2020-01-09 17:04:53 +0530

    Added PAT expire in days and automated long period PAT control

--- src/AzSK.AzureDevOps/Framework/Core/SVT/AzureDevOps/AzureDevOps.User.ps1
+++ src/AzSK.AzureDevOps/Framework/Core/SVT/AzureDevOps/AzureDevOps.User.ps1
@@ -11,7 +11,7 @@ class User: SVTBase
 	{
         $apiURL = "https://{0}.vssps.visualstudio.com/_apis/Token/SessionTokens?displayFilterOption=1&createdByOption=3&sortByOption=3&isSortAscending=false&startRowNumber=1&pageSize=100&api-version=5.0-preview.1" -f $($this.SubscriptionContext.SubscriptionName);
         $responseObj = [WebRequestHelper]::InvokeGetWebRequest($apiURL);
-
+        try {
         if($responseObj.Count -gt 0)
         {
             $fullAccessPATList =   $responseObj | Where-Object {$_.scope -eq "app_token"}
@@ -32,6 +32,12 @@ class User: SVTBase
             $controlResult.AddMessage([VerificationResult]::Passed,
                                         "No PAT token found");
         }
+                    
+       }
+       catch {
+           $controlResult.AddMessage([VerificationResult]::Passed,
+                                           "No PAT token found");
+       }
         
         return $controlResult;
     }
@@ -61,6 +67,90 @@ class User: SVTBase
                                                 "Alt credential not found");
         }
         return $controlResult
+    }
+
+    hidden [ControlResult] ValidatePATExpiryPeriod([ControlResult] $controlResult)
+	{
+      try {
+
+        $apiURL = "https://{0}.vssps.visualstudio.com/_apis/Token/SessionTokens?displayFilterOption=1&createdByOption=3&sortByOption=3&isSortAscending=false&startRowNumber=1&pageSize=100&api-version=5.0-preview.1" -f $($this.SubscriptionContext.SubscriptionName);
+        $responseObj = [WebRequestHelper]::InvokeGetWebRequest($apiURL);
+
+        if($responseObj.Count -gt 0)
+        { #([datetime]::parseexact($responseObj[0].validto.Split('T')[0], 'yyyy-MM-dd', $null) - [datetime]::parseexact($responseObj[0].validfrom.Split('T')[0], 'yyyy-MM-dd', $null)).Days
+            $AccessPATList =    $responseObj | Where-Object {$_.validto -gt $(Get-Date -Format "yyyy-MM-dd")}
+           
+            if(($AccessPATList | Measure-Object).Count -gt 0)
+            {
+                $res = $responseObj | Where-Object {(([datetime]::parseexact($_.validto.Split('T')[0], 'yyyy-MM-dd', $null) - [datetime]::parseexact($_.validfrom.Split('T')[0], 'yyyy-MM-dd', $null)).Days) -gt 180}
+                
+                if(($res | Measure-Object).Count -gt 0)
+                {
+                 $PATList =($AccessPATList | Select-Object -Property @{Name="Name"; Expression = {$_.displayName}},@{Name="ValidFrom"; Expression = {$_.validfrom}},@{Name="ValidTo"; Expression = {$_.validto}},@{Name="ValidationPeriod"; Expression = {([datetime]::parseexact($_.validto.Split('T')[0], 'yyyy-MM-dd', $null) - [datetime]::parseexact($_.validfrom.Split('T')[0], 'yyyy-MM-dd', $null)).Days}});    
+                 $controlResult.AddMessage([VerificationResult]::Failed, "PAT tokens with validation period more than 180 days",$PATList)  
+                }
+            }
+            else {
+                $controlResult.AddMessage([VerificationResult]::Passed,
+                                         "No PAT token found with validation perios more than 180 days")  
+            }
+        }
+        else
+        {
+            $controlResult.AddMessage([VerificationResult]::Passed,
+                                        "No PAT token found");
+        }
+       }
+       catch {
+        $controlResult.AddMessage([VerificationResult]::Manual,
+        "Not able to find PAT token");
+       }
+        
+        return $controlResult;
+    }
+    hidden [ControlResult] CheckPATExpiration([ControlResult] $controlResult)
+	{
+      try {
+
+        $apiURL = "https://{0}.vssps.visualstudio.com/_apis/Token/SessionTokens?displayFilterOption=1&createdByOption=3&sortByOption=3&isSortAscending=false&startRowNumber=1&pageSize=100&api-version=5.0-preview.1" -f $($this.SubscriptionContext.SubscriptionName);
+        $responseObj = [WebRequestHelper]::InvokeGetWebRequest($apiURL);
+
+        if($responseObj.Count -gt 0)
+        { #([datetime]::parseexact($responseObj[0].validto.Split('T')[0], 'yyyy-MM-dd', $null) - [datetime]::parseexact($responseObj[0].validfrom.Split('T')[0], 'yyyy-MM-dd', $null)).Days
+           $date = Get-Date;
+           $date.AddDays(10).ToString("yyyy-MM-dd")
+           $AccessPATList =    $responseObj | Where-Object {$_.validto -gt $(Get-Date -Format "yyyy-MM-dd")}
+           
+            if(($AccessPATList | Measure-Object).Count -gt 0)
+            {
+                $res = $responseObj | Where-Object {(([datetime]::parseexact($_.validto.Split('T')[0], 'yyyy-MM-dd', $null) - $date).Days) -lt 8}
+                
+                if(($res | Measure-Object).Count -gt 0)
+                {
+                 $PATList =($AccessPATList | Select-Object -Property @{Name="Name"; Expression = {$_.displayName}},@{Name="ValidFrom"; Expression = {$_.validfrom}},@{Name="ValidTo"; Expression = {$_.validto}},@{Name="Remaining"; Expression = {([datetime]::parseexact($_.validto.Split('T')[0], 'yyyy-MM-dd', $null) - $date).Days}});    
+                 $controlResult.AddMessage([VerificationResult]::Failed, "PAT tokens which expire within 7 days",$PATList)  
+                }
+                else {
+                    $controlResult.AddMessage([VerificationResult]::Passed, "No PAT tokens found which expire within 7 days")
+                }
+            }
+            else {
+                $controlResult.AddMessage([VerificationResult]::Passed,
+                                         "No active PAT token found")  
+            }
+        }
+        else
+        {
+            $controlResult.AddMessage([VerificationResult]::Passed,
+                                        "No PAT token found");
+        }
+       }
+       catch {
+        $controlResult.AddMessage([VerificationResult]::Manual,
+        "Not able to find PAT token");
+       }
+        
+        return $controlResult;
     }
 
 }
