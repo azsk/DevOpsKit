90119138bfa42b39ec6d47a84893dab8bcea8a76
Zhilmil Gupta (Tata Consultancy Services Ltd) <v-zhgup@microsoft.com>
2020-01-22 21:19:27 +0530

    Virtual Network base object code fix

--- src/AzSK/Framework/Core/SVT/SVTIaasBase.ps1
+++ src/AzSK/Framework/Core/SVT/SVTIaasBase.ps1
@@ -38,94 +38,105 @@ class SVTIaasBase: AzSVTBase
 
 	hidden [PSObject[]] GetvNetNics($VNetSubnets)
     {
-        if (-not $this.vNetNics)
-		{
-			$this.vNetNicsWIssues = @();
-			$VNetSubnets | ForEach-Object{
-				Set-Variable -Name currentsubnet -Scope Local -Value $_
-				if($null -ne $currentsubnet.IpConfigurations )
+		
+			if (-not $this.vNetNics)
+			{
+				$nics = @();
+				$ipconf=@(); 
+				$nicResID = @();				
+				$nics +=  Get-AzNetworkInterface # TODO: if possible get the resources from the base object
+				$ipconf += ($VNetSubnets| Select-Object -Property 'IpConfigurations' -ExpandProperty 'IpConfigurations') 
+				foreach($ipc in $ipconf)
 				{
-						$currentsubnet.IpConfigurations | ForEach-Object{
-						Set-Variable -Name currentipconfig -Scope Local -Value $_
-						if($currentipconfig.Id.Contains("Microsoft.Network/networkInterfaces"))
+						$ipcId = $ipc.Id.ToString();
+						if($ipcId.Contains("Microsoft.Network/networkInterfaces"))
 						{
-								$currentipconfig = $currentipconfig.Id.ToLower()
-								$nicresourceid =  $currentipconfig.Substring(0,$currentipconfig.LastIndexOf("ipconfigurations")-1)
-								try
-								{
-									#<TODO: Perf Issue - Get-AzResource is called in foreach which will Provider list and perform issue. Resource Ids can be passed from base location>
-									$nic = Get-AzResource -ResourceId $nicresourceid
-									$this.vNetNics += $nic
-								}
-								catch
-								{
-									$this.vNetNicsWIssues += $nicresourceid;
-								}								
+							$nicResID += $ipcId.Substring(0,($ipcId.LastIndexOf("ipConfigurations")-1))
 						}
-					}
 				}
-			}
-        }
+				
+				if($nicResID.Count -gt 0)
+				{
+					$this.VNetNics += $nics | Where-Object{$_.Id -in $nicResID}
+				}
+								
+             }	
+			
+		
         return $this.vNetNics;
     }
 
 	hidden [PSObject[]] GetvnetNicsProperties($vNetNics)
 	{
+
+		
 		if(-not $this.vNetNicsOutput)
 		{
 			if($null -ne $vNetNics )
 			{
 				$this.vNetPIPIssues = @();
 				$tempVNetNICS = [array]($vNetNics)
-				$tempVNetNICS | ForEach-Object{
-					Set-Variable -Name nic -Scope Local -Value $_
-					Set-Variable -Name nicproperties -Scope Local -Value $_.Properties
+				foreach($tempnic in $tempVNetNICS)
+				{
 					try
 					{
-						$out = ""| Select-Object NICName, VMName, VMId, PrimaryStatus, NetworkSecurityGroupName,NetworkSecurityGroupId, PublicIpAddress, PrivateIpAddress,  EnableIPForwarding, IpConfigurations
-						$out.NICName = $nic.Name
-						$out.IpConfigurations = $nicproperties.IpConfigurations
-						$out.EnableIPForwarding = $nicproperties.EnableIPForwarding
+						Set-Variable -Name nic -Scope Local -Value $tempnic 
+                       	$out = ""| Select-Object NICName, VMName, VMId, PrimaryStatus, NetworkSecurityGroupName,NetworkSecurityGroupId, PublicIpAddress, PrivateIpAddress,  EnableIPForwarding, IpConfigurations
+						$out.NICName = $tempnic.Name
+						$out.IpConfigurations = $tempnic.IpConfigurations
+						$out.EnableIPForwarding = $tempnic.EnableIPForwarding
 						$PublicIpAddresses = @()
 						$PrivateIpAddresses = @()
-						$nicproperties.IpConfigurations | ForEach-Object{
-							Set-Variable -Name ipconfiguration -Scope Local -Value $_
-							try
+											
+							$NICPublicIpAddresses = @();
+							$NICPublicIpAddresses +=  $tempnic.ipconfigurations | Where-Object {$null -ne $_.PublicIpAddress}
+							$PrivateIpAddresses += $tempnic.ipconfigurations.PrivateIpAddress
+							if(($NICPublicIpAddresses |Measure-Object).Count -gt 0)
 							{
-								if(($ipconfiguration | Get-Member -Name "Properties") -and ($ipconfiguration.Properties | Get-Member -Name "PublicIpAddress") -and $ipconfiguration.Properties.PublicIpAddress)
-								{
-									$IPResource = Get-AzResource -ResourceId $ipconfiguration.Properties.PublicIpAddress.Id
+								foreach($nicwithPublicAddress in $NICPublicIpAddresses) 
+								{	try
+									{
+					
+									$IPResource = Get-AzResource -ResourceId $nicwithPublicAddress.PublicIpAddress.Id
 									$pubResourceName = Get-AzPublicIpAddress -Name $IPResource.Name -ResourceGroupName $IPResource.ResourceGroupName
 									$PublicIpAddresses += $pubResourceName.IpAddress
+									}
+									catch
+									{
+										
+										$this.vNetPIPIssues += $tempnic.IpConfigurations
+									}
+											
+								
 								}
-								$PrivateIpAddresses += $ipconfiguration.Properties.PrivateIpAddress
-							}
-							catch
-							{
-								$this.vNetPIPIssues += $ipconfiguration
-							}
-						}
-						$out.PublicIpAddress = ([System.String]::Join(";",$PublicIpAddresses))
-						$out.PrivateIpAddress = ([System.String]::Join(";",$PrivateIpAddresses))
+						    }
 
-						if(($nicproperties | Get-Member -Name "VirtualMachine") -and $nicproperties.VirtualMachine )
+			
+						
+					
+						$out.PublicIpAddress = ([System.String]::Join(";",$PublicIpAddresses))
+						$out.PrivateIpAddress = ([System.String]::Join(";",$PrivateIpAddresses))					
+
+						if(($tempnic | Get-Member -Name "VirtualMachine") -and $tempnic.VirtualMachine )
 						{
-							$vmresource = Get-AzResource -ResourceId $nicproperties.VirtualMachine.Id
-							$out.VMName = $vmresource.Name
+							$tempobj = ($tempnic.VirtualMachine.Id).split('/')
+                            $out.VMName = $tempobj[$($tempobj.Count)-1]
 						}
 						else {
 							$out.VMName = ""
 						}
-						if($null -ne ($nicproperties | Get-Member primary))
+						if($null -ne ($tempnic | Get-Member primary))
 						{
-							$out.PrimaryStatus = $nicproperties.primary
+							$out.PrimaryStatus = $tempnic.primary
 						}
 
-						if(($nicproperties | Get-Member -Name "NetworkSecurityGroup") -and $nicproperties.NetworkSecurityGroup)
+						if(($tempnic | Get-Member -Name "NetworkSecurityGroup") -and $tempnic.NetworkSecurityGroup)
 						{
-							$nsgresource = Get-AzResource -ResourceId $nicproperties.NetworkSecurityGroup.Id
-							$out.NetworkSecurityGroupName = $nsgresource.Name
+							$out.NetworkSecurityGroupId = $tempnic.NetworkSecurityGroup.Id
+                            $tempobj = ($tempnic.NetworkSecurityGroup.Id).split('/')
+                            $out.NetworkSecurityGroupName = $tempobj[$($tempobj.Count)-1]
 						}
+
 						$this.vNetNicsOutput += $out
 					}
 					catch
--- src/AzSK/Framework/Core/SVT/Services/VirtualNetwork.ps1
+++ src/AzSK/Framework/Core/SVT/Services/VirtualNetwork.ps1
@@ -12,13 +12,14 @@ class VirtualNetwork: SVTIaasBase
     {
         if($null -ne $this.vNetNicsOutput)
         {
+            $PublicIps = @();
 			$controlResult.AddMessage([MessageData]::new("Analyzing all the NICs configured in the VNet"));
-            $PublicIpCount = (($this.vNetNicsOutput | Where-Object {!([System.String]::IsNullOrWhiteSpace($_.PublicIpAddress))}) | Measure-Object).count
-            if($PublicIpCount -gt 0)
+            $PublicIps += ($this.vNetNicsOutput | Where-Object {!([System.String]::IsNullOrWhiteSpace($_.PublicIpAddress))})
+            if($PublicIps.Count -gt 0)
             {
 				$publicIPList = @()
 				$controlResult.AddMessage([VerificationResult]::Verify, [MessageData]::new("Verify below Public IP(s) on the Vnet"));
-                $this.vNetNicsOutput | ForEach-Object{
+                $PublicIps | ForEach-Object{
                     Set-Variable -Name nic -Scope Local -Value $_
 					$publicIP = $nic | Select-Object NICName, VMName, PrimaryStatus, NetworkSecurityGroupName, PublicIpAddress, PrivateIpAddress
 					$publicIPList += $publicIP
