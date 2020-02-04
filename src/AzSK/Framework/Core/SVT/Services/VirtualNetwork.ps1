5eceab9647b24aa3e154cb534284d3bae787eb6b
Zhilmil Gupta (Tata Consultancy Services Ltd) <v-zhgup@microsoft.com>
2020-02-04 23:50:37 +0530

    Removing last commited changes

--- src/AzSK/Framework/Core/SVT/Services/VirtualNetwork.ps1
+++ src/AzSK/Framework/Core/SVT/Services/VirtualNetwork.ps1
@@ -1,249 +1,205 @@
-﻿Set-StrictMode -Version Latest
-class SVTIaasBase: AzSVTBase
+﻿#using namespace Microsoft.Azure.Commands.VirtualNetwork.Models
+Set-StrictMode -Version Latest
+class VirtualNetwork: SVTIaasBase
 {
-	hidden [PSObject] $ResourceObject;
-	hidden [PSObject[]] $vNetNics;
-	hidden [PSObject[]] $vNetNicsWIssues;
-	hidden [PSObject[]] $vNetPIPIssues;
-	hidden [PSObject[]] $vNetNicsOutput;
 
-	SVTIaasBase([string] $subscriptionId, [string] $resourceGroupName, [string] $resourceName):
-        Base($subscriptionId, $resourceGroupName, $resourceName)
-    {
-        $this.GetResourceObject();
-		$this.GetvNetNics($this.ResourceObject.Subnets);
-		$this.GetvnetNicsProperties($this.vNetNics);
-    }
-
-	SVTIaasBase([string] $subscriptionId, [SVTResource] $svtResource):
+	VirtualNetwork([string] $subscriptionId, [SVTResource] $svtResource):
         Base($subscriptionId, $svtResource)
     {
-		$this.GetResourceObject();
-		$this.GetvNetNics($this.ResourceObject.Subnets);
-		$this.GetvnetNicsProperties($this.vNetNics);
     }
 
-	hidden [PSObject] GetResourceObject()
+	hidden [ControlResult] CheckPublicIps([ControlResult] $controlResult)
     {
-        if (-not $this.ResourceObject) {
-            $this.ResourceObject = Get-AzVirtualNetwork -ResourceGroupName $this.ResourceContext.ResourceGroupName `
-											 -Name $this.ResourceContext.ResourceName
-            if(-not $this.ResourceObject)
+        if($null -ne $this.vNetNicsOutput)
+        {
+            $PublicIps = @();
+			$controlResult.AddMessage([MessageData]::new("Analyzing all the NICs configured in the VNet"));
+            $PublicIps += ($this.vNetNicsOutput | Where-Object {!([System.String]::IsNullOrWhiteSpace($_.PublicIpAddress))})
+            if($PublicIps.Count -gt 0)
             {
-                throw ([SuppressedException]::new(("Resource '{0}' not found under Resource Group '{1}'" -f ($this.ResourceContext.ResourceName), ($this.ResourceContext.ResourceGroupName)), [SuppressedExceptionType]::InvalidOperation))
+				$publicIPList = @()
+				$controlResult.AddMessage([VerificationResult]::Verify, [MessageData]::new("Verify below Public IP(s) on the Vnet"));
+                $PublicIps | ForEach-Object{
+                    Set-Variable -Name nic -Scope Local -Value $_
+					$publicIP = $nic | Select-Object NICName, VMName, PrimaryStatus, NetworkSecurityGroupName, PublicIpAddress, PrivateIpAddress
+					$publicIPList += $publicIP
+					$controlResult.AddMessage([MessageData]::new($publicIP));
+                }
+
+				$controlResult.SetStateData("Public IP(s) on the Vnet", $publicIPList);
+            }
+            else
+            {
+				$controlResult.AddMessage([VerificationResult]::Passed, [MessageData]::new("No Public IP is configured in any NIC on the Vnet"));
             }
         }
-        return $this.ResourceObject;
+        else
+        {
+			$controlResult.AddMessage([VerificationResult]::Passed, [MessageData]::new("No NICs found on the VNet with Public IP configured"));
+        }
+
+        return $controlResult;
     }
 
-	hidden [PSObject[]] GetvNetNics($VNetSubnets)
+	hidden [ControlResult] CheckIPForwardingforNICs([ControlResult] $controlResult)
     {
-		if([FeatureFlightingManager]::GetFeatureStatus("EnableVnetFixForSub",$($this.SubscriptionContext.SubscriptionId)))
+        if($null -ne $this.vNetNicsOutput -and ($this.vNetNicsOutput | Measure-Object).count -gt 0)
 		{
-			if (-not $this.vNetNics)
+			[array] $vNetNicsIPFwed = $this.vNetNicsOutput | Where-Object { $_.EnableIPForwarding }
+
+			if($null -ne $vNetNicsIPFwed -and ($vNetNicsIPFwed | Measure-Object).count -gt 0)
 			{
-				$this.vNetNicsWIssues = @();
-				$VNetSubnets | ForEach-Object{
-					Set-Variable -Name currentsubnet -Scope Local -Value $_
-					if($null -ne $currentsubnet.IpConfigurations )
-					{
-							$currentsubnet.IpConfigurations | ForEach-Object{
-							Set-Variable -Name currentipconfig -Scope Local -Value $_
-							if($currentipconfig.Id.Contains("Microsoft.Network/networkInterfaces"))
-							{
-									$currentipconfig = $currentipconfig.Id.ToLower()
-									$nicresourceid =  $currentipconfig.Substring(0,$currentipconfig.LastIndexOf("ipconfigurations")-1)
-									try
-									{
-										#<TODO: Perf Issue - Get-AzResource is called in foreach which will Provider list and perform issue. Resource Ids can be passed from base location>
-										$nic = Get-AzResource -ResourceId $nicresourceid
-										$this.vNetNics += $nic
-									}
-									catch
-									{
-										$this.vNetNicsWIssues += $nicresourceid;
-									}								
-							}
-						}
-					}
-				}
+				$controlResult.AddMessage([VerificationResult]::Verify, [MessageData]::new("Verify IP Forwarding is enabled for below NIC(s) in VNet"));
+				$controlResult.AddMessage([MessageData]::new(($vNetNicsIPFwed | Select-Object NICName, EnableIPForwarding)));
+				$controlResult.SetStateData("IP Forwarding is enabled for NIC(s) in VNet", $vNetNicsIPFwed);
 			}
-			return $this.vNetNics;
+			else
+			{
+				$controlResult.AddMessage([VerificationResult]::Passed, [MessageData]::new("There are no NICs with EnableIPForwarding turned on the VNet"));
+			}
 		}
 		else
 		{
-			if (-not $this.vNetNics)
-			{
-				$this.vNetNicsWIssues = @();
-				$VNetSubnets | ForEach-Object{
-					Set-Variable -Name currentsubnet -Scope Local -Value $_
-					if($null -ne $currentsubnet.IpConfigurations )
-					{
-							$currentsubnet.IpConfigurations | ForEach-Object{
-							Set-Variable -Name currentipconfig -Scope Local -Value $_
-							if($currentipconfig.Id.Contains("Microsoft.Network/networkInterfaces"))
-							{
-									$currentipconfig = $currentipconfig.Id.ToLower()
-									$nicresourceid =  $currentipconfig.Substring(0,$currentipconfig.LastIndexOf("ipconfigurations")-1)
-									try
-									{
-										#<TODO: Perf Issue - Get-AzResource is called in foreach which will Provider list and perform issue. Resource Ids can be passed from base location>
-										$nic = Get-AzResource -ResourceId $nicresourceid
-										$this.vNetNics += $nic
-									}
-									catch
-									{
-										$this.vNetNicsWIssues += $nicresourceid;
-									}								
-							}
-						}
-					}
-				}
-			}
-			return $this.vNetNics;			
+			$controlResult.AddMessage([VerificationResult]::Passed, [MessageData]::new("There are no NICs with EnableIPForwarding turned on the VNet"));
 		}
+
+        return $controlResult;
     }
 
-	hidden [PSObject[]] GetvnetNicsProperties($vNetNics)
-	{
-		if([FeatureFlightingManager]::GetFeatureStatus("EnableVnetFixForSub",$($this.SubscriptionContext.SubscriptionId)))
-		{	if(-not $this.vNetNicsOutput)
-			{
-				if($null -ne $vNetNics )
-				{
-					$this.vNetPIPIssues = @();
-					$tempVNetNICS = [array]($vNetNics)
-					$tempVNetNICS | ForEach-Object{
-						Set-Variable -Name nic -Scope Local -Value $_
-						Set-Variable -Name nicproperties -Scope Local -Value $_.Properties
-						try
-						{
-							$out = ""| Select-Object NICName, VMName, VMId, PrimaryStatus, NetworkSecurityGroupName,NetworkSecurityGroupId, PublicIpAddress, PrivateIpAddress,  EnableIPForwarding, IpConfigurations
-							$out.NICName = $nic.Name
-							$out.IpConfigurations = $nicproperties.IpConfigurations
-							$out.EnableIPForwarding = $nicproperties.EnableIPForwarding
-							$PublicIpAddresses = @()
-							$PrivateIpAddresses = @()
-							$nicproperties.IpConfigurations | ForEach-Object{
-								Set-Variable -Name ipconfiguration -Scope Local -Value $_
-								try
-								{
-									if(($ipconfiguration | Get-Member -Name "Properties") -and ($ipconfiguration.Properties | Get-Member -Name "PublicIpAddress") -and $ipconfiguration.Properties.PublicIpAddress)
+	hidden [ControlResult] CheckNSGUseonGatewaySubnet([ControlResult] $controlResult)
+    {
+        $gateWaySubnet = $this.ResourceObject.Subnets | Where-Object {$_.Name -eq "GatewaySubnet"}
+        if($null -ne $gateWaySubnet)
+        {
+            if($null-ne $gateWaySubnet.NetworkSecurityGroup  -and -not [System.String]::IsNullOrWhiteSpace($gateWaySubnet.NetworkSecurityGroup.Id))
+            {
+				$controlResult.AddMessage([VerificationResult]::Failed, [MessageData]::new("NSG is configured on the Gateway Subnet of VNet", ($gateWaySubnet | Select-Object Name, NetworkSecurityGroupText)));
+				$controlResult.SetStateData("Gateway subnet of VNet", ($gateWaySubnet | Select-Object Name, NetworkSecurityGroup));
+            }
+            else
+            {
+				$controlResult.AddMessage([VerificationResult]::Passed, [MessageData]::new("There are no NSG's configured on the Gateway subnet of VNet"));
+            }
+        }
+        else
+        {
+			$controlResult.AddMessage([VerificationResult]::Passed, [MessageData]::new("No Gateway subnet found on the VNet"));
+        }
+
+        return $controlResult;
+    }
+
+	hidden [ControlResult] CheckNSGConfigured([ControlResult] $controlResult)
+    {
+        $hasTCPPassed = $true
+        if($null -ne $this.ResourceObject.Subnets)
+        {
+            $subnetsWithoutNSG = $this.ResourceObject.Subnets | Where-Object {$null -eq $_.NetworkSecurityGroup -and $_.Name.ToLower() -ne "gatewaysubnet"}
+            $subnetsWithNSG = $this.ResourceObject.Subnets | Where-Object {$null -ne $_.NetworkSecurityGroup -and $_.Name.ToLower() -ne "gatewaysubnet"}
+
+            if($null-ne $subnetsWithoutNSG  -and ($subnetsWithoutNSG | Measure-Object).count -gt 0)
+            {
+				$hasTCPPassed = $false
+				$controlResult.AddMessage([VerificationResult]::Failed, [MessageData]::new("NSG is not configured for below subnet(s) of VNet", ($subnetsWithoutNSG | Select-Object Name, Id)));
+            }
+            if($null-ne $subnetsWithNSG  -and ($subnetsWithNSG | Measure-Object).Count -gt 0)
+            {
+                #$checkSubnets = @()
+                $InvalidRulesList = @()
+                $subnetsWithNSG | ForEach-Object{
+							$nsgid = $_.NetworkSecurityGroup.Id
+							$nsglist = Get-AzResource -ResourceId $nsgid
+
+							$nsglist | ForEach-Object{
+								$rules = $_.Properties.SecurityRules
+								$rules | ForEach-Object{
+									$ruleproperties = $_.Properties
+									if((($ruleproperties.Direction -eq "outbound") -or ($ruleproperties.Direction -eq "inbound")) -and (([Helpers]::CheckMember($ruleproperties,"SourceAddressPrefix")) -and $ruleproperties.SourceAddressPrefix -eq '*') -and $ruleproperties.DestinationAddressPrefix -eq '*' -and $ruleproperties.Access -eq "allow")
 									{
-										$IPResource = Get-AzResource -ResourceId $ipconfiguration.Properties.PublicIpAddress.Id
-										$pubResourceName = Get-AzPublicIpAddress -Name $IPResource.Name -ResourceGroupName $IPResource.ResourceGroupName
-										$PublicIpAddresses += $pubResourceName.IpAddress
+										$InvalidRulesList += $_ | Select-Object Id, Properties
 									}
-									$PrivateIpAddresses += $ipconfiguration.Properties.PrivateIpAddress
-								}
-								catch
-								{
-									$this.vNetPIPIssues += $ipconfiguration
 								}
 							}
-							$out.PublicIpAddress = ([System.String]::Join(";",$PublicIpAddresses))
-							$out.PrivateIpAddress = ([System.String]::Join(";",$PrivateIpAddresses))
+					$currentsubnet=$_
+                    if(($InvalidRulesList | Measure-Object).Count -gt 0)
+                    {
+						$controlResult.SetStateData("Potentially dangerous any to any outbound security rule(s) found in subnet - ["+ $_.Name +"]", $InvalidRulesList);
+						$hasTCPPassed = $false
+						$controlResult.AddMessage([VerificationResult]::Failed, [MessageData]::new("Potentially dangerous any to any outbound security rule(s) found in subnet - ["+ $_.Name +"]", $InvalidRulesList));
+                    }
+                }
+            }
+        }
+        else
+        {
+			$controlResult.AddMessage([VerificationResult]::Passed, [MessageData]::new("No subnets found on VNet"));
+        }
 
-							if(($nicproperties | Get-Member -Name "VirtualMachine") -and $nicproperties.VirtualMachine )
-							{
-								$vmresource = Get-AzResource -ResourceId $nicproperties.VirtualMachine.Id
-								$out.VMName = $vmresource.Name
-							}
-							else {
-								$out.VMName = ""
-							}
-							if($null -ne ($nicproperties | Get-Member primary))
-							{
-								$out.PrimaryStatus = $nicproperties.primary
-							}
+		if($hasTCPPassed -eq $true)
+        {
+			  $controlResult.VerificationResult = [VerificationResult]::Passed;
+        }
 
-							if(($nicproperties | Get-Member -Name "NetworkSecurityGroup") -and $nicproperties.NetworkSecurityGroup)
-							{
-								$nsgresource = Get-AzResource -ResourceId $nicproperties.NetworkSecurityGroup.Id
-								$out.NetworkSecurityGroupName = $nsgresource.Name
-							}
-							$this.vNetNicsOutput += $out
-						}
-						catch
+        return $controlResult;
+    }
+
+	hidden [ControlResult] CheckGatewayUsed([ControlResult] $controlResult)
+    {
+        $vNetGateways = @()
+        $VNetSubnets = $this.ResourceObject.Subnets
+        if($null -ne $VNetSubnets)
+        {
+            $VNetSubnets | ForEach-Object{
+                Set-Variable -Name currentsubnet -Scope Local -Value $_
+                if($null -ne $currentsubnet.IpConfigurations )
+                {
+                    $currentsubnet.IpConfigurations | ForEach-Object{
+						Set-Variable -Name currentipconfig -Scope Local -Value $_
+						if($currentipconfig.Id.ToLower().Contains("microsoft.network/virtualnetworkgateways"))
 						{
-							#eat the exception. Error nic is already added to the list
+							$currentipconfig = $currentipconfig.Id.ToLower()
+							$gatewayresourceid =  $currentipconfig.Substring(0,$currentipconfig.LastIndexOf("ipconfigurations")-1)
+							$gateway = Get-AzResource -ResourceId $gatewayresourceid
+
+							$vNetGateway = New-Object System.Object
+							$vNetGateway | Add-Member -type NoteProperty -name ResourceName -Value $gateway.Name
+							$vNetGateway | Add-Member -type NoteProperty -name ResourceGroupName -Value $gateway.ResourceGroupName
+							$vNetGateway | Add-Member -type NoteProperty -name GatewayType -Value $gateway.Properties.gatewayType
+							$vNetGateway | Add-Member -type NoteProperty -name VPNType -Value $gateway.Properties.vpnType
+
+							$vNetGateways += $vNetGateway
 						}
-					}
-					$this.vNetNicsOutput = [array]($this.vNetNicsOutput)
-				}
+                    }
+                }
 			}
-			return $this.vNetNicsOutput;
-		}
-		else
-		{
-			if(-not $this.vNetNicsOutput)
-			{
-				if($null -ne $vNetNics )
-				{
-					$this.vNetPIPIssues = @();
-					$tempVNetNICS = [array]($vNetNics)
-					$tempVNetNICS | ForEach-Object{
-						Set-Variable -Name nic -Scope Local -Value $_
-						Set-Variable -Name nicproperties -Scope Local -Value $_.Properties
-						try
-						{
-							$out = ""| Select-Object NICName, VMName, VMId, PrimaryStatus, NetworkSecurityGroupName,NetworkSecurityGroupId, PublicIpAddress, PrivateIpAddress,  EnableIPForwarding, IpConfigurations
-							$out.NICName = $nic.Name
-							$out.IpConfigurations = $nicproperties.IpConfigurations
-							$out.EnableIPForwarding = $nicproperties.EnableIPForwarding
-							$PublicIpAddresses = @()
-							$PrivateIpAddresses = @()
-							$nicproperties.IpConfigurations | ForEach-Object{
-								Set-Variable -Name ipconfiguration -Scope Local -Value $_
-								try
-								{
-									if(($ipconfiguration | Get-Member -Name "Properties") -and ($ipconfiguration.Properties | Get-Member -Name "PublicIpAddress") -and $ipconfiguration.Properties.PublicIpAddress)
-									{
-										$IPResource = Get-AzResource -ResourceId $ipconfiguration.Properties.PublicIpAddress.Id
-										$pubResourceName = Get-AzPublicIpAddress -Name $IPResource.Name -ResourceGroupName $IPResource.ResourceGroupName
-										$PublicIpAddresses += $pubResourceName.IpAddress
-									}
-									$PrivateIpAddresses += $ipconfiguration.Properties.PrivateIpAddress
-								}
-								catch
-								{
-									$this.vNetPIPIssues += $ipconfiguration
-								}
-							}
-							$out.PublicIpAddress = ([System.String]::Join(";",$PublicIpAddresses))
-							$out.PrivateIpAddress = ([System.String]::Join(";",$PrivateIpAddresses))
+        }
 
-							if(($nicproperties | Get-Member -Name "VirtualMachine") -and $nicproperties.VirtualMachine )
-							{
-								$vmresource = Get-AzResource -ResourceId $nicproperties.VirtualMachine.Id
-								$out.VMName = $vmresource.Name
-							}
-							else {
-								$out.VMName = ""
-							}
-							if($null -ne ($nicproperties | Get-Member primary))
-							{
-								$out.PrimaryStatus = $nicproperties.primary
-							}
+        if($null -ne $vNetGateways -and ($vNetGateways | Measure-Object).Count -gt 0)
+        {
+            $controlResult.AddMessage([VerificationResult]::Verify, [MessageData]::new("Verify below virtual network gateways found on VNet", $vNetGateways));
+			$controlResult.SetStateData("Virtual Network Gateways found on VNet", $vNetGateways);
+        }
+        else
+        {
+			$controlResult.AddMessage([VerificationResult]::Passed, [MessageData]::new("No gateways found in VNet"));
+        }
 
-							if(($nicproperties | Get-Member -Name "NetworkSecurityGroup") -and $nicproperties.NetworkSecurityGroup)
-							{
-								$nsgresource = Get-AzResource -ResourceId $nicproperties.NetworkSecurityGroup.Id
-								$out.NetworkSecurityGroupName = $nsgresource.Name
-							}
-							$this.vNetNicsOutput += $out
-						}
-						catch
-						{
-							#eat the exception. Error nic is already added to the list
-						}
-					}
-					$this.vNetNicsOutput = [array]($this.vNetNicsOutput)
-				}
-			}
-			return $this.vNetNicsOutput;
-		}
+        return $controlResult;
+    }
 
-		
-	}
+	hidden [ControlResult] CheckVnetPeering([ControlResult] $controlResult)
+    {
+        $vnetPeerings = Get-AzVirtualNetworkPeering -VirtualNetworkName $this.ResourceContext.ResourceName -ResourceGroupName $this.ResourceContext.ResourceGroupName
+        if($null -ne $vnetPeerings -and ($vnetPeerings|Measure-Object).count -gt 0)
+        {
+			$controlResult.AddMessage([VerificationResult]::Verify, [MessageData]::new("Verify below peering found on VNet", $vnetPeerings));
+			$controlResult.SetStateData("Peering found on VNet", $vnetPeerings);
+        }
+        else
+        {
+			$controlResult.AddMessage([VerificationResult]::Passed, [MessageData]::new("No VNet peering found on VNet", $vnetPeerings));
+        }
+
+        return $controlResult;
+    }
 }
