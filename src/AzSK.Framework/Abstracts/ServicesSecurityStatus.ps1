0dd9bb8b08af3296261f9ebb6d87397c138d1661
Garima Singh <v-garisi@microsoft.com>
2020-04-02 16:50:01 +0530

    ASC and GXX FilterTags Bug fix

--- src/AzSK.Framework/Abstracts/ServicesSecurityStatus.ps1
+++ src/AzSK.Framework/Abstracts/ServicesSecurityStatus.ps1
@@ -554,7 +554,12 @@ class ServicesSecurityStatus: AzSVTCommandBase
 					$controlIdsWithFilterTagList += $controlList | Where-Object{ $tagName -in $_.Tags  } | ForEach-Object{ $_.ControlId}
 				}
 				#Assign filtered control Id with tag name 
-				$this.ControlIds = $controlIdsWithFilterTagList
+				if(($this.ControlIds | Measure-Object).Count -gt 0)
+				{
+					#If controlids are already present in the object then intersection of controlids should be scanned
+					$controlIdsWithFilterTagList = $controlIdsWithFilterTagList | where {$this.ControlIds -Contains $_}
+					$this.ControlIds = $controlIdsWithFilterTagList
+				}
 			}
 
 			#********** Commentiing Exclude tags logic as this will not require perf optimization as excludeTags mostly will result in most of the resources
