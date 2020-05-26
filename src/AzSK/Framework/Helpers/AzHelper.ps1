683f7e688ddd9b745dd945e0f232ca4f1c8de30e
Rohit Yadav <RohitYadav-msft>
2020-05-26 14:58:55 +0530

    Removing "IsSetAzStorageBlobAvailable" feature flag

--- src/AzSK/Framework/Helpers/AzHelper.ps1
+++ src/AzSK/Framework/Helpers/AzHelper.ps1
@@ -2,20 +2,7 @@ class AzHelper
 {
 static [void] UploadStorageBlobContent([string] $fileName, [string] $blobName, [string] $containerName, [object] $stgCtx)
 	{
-        if([FeatureFlightingManager]::GetFeatureStatus("IsSetAzStorageBlobAvailable","*") -eq $true)
-        {
-            Set-AzStorageBlobContent -Blob $blobName -Container $containerName -File $fileName -Context $stgCtx -Force | Out-Null
-        }
-        else {
-            $blob = $stgCtx.StorageAccount.CreateCloudBlobClient().GetContainerReference($containerName).GetBlockBlobReference($blobName)
-            $task = $blob.UploadFromFileAsync($fileName)
-            $task.Wait()
-            if (-not ($task.IsCompleted -and !$task.IsFaulted))
-            {
-				#Need to change write method
-				Write-Debug "Transferring file" + $fileName + "to storage has failed!!"
-            }
-        }
+        Set-AzStorageBlobContent -Blob $blobName -Container $containerName -File $fileName -Context $stgCtx -Force | Out-Null
     }
 
     static [object] GetStorageBlobContent([string] $folderName, [string] $fileName, [string] $blobName, [string] $containerName, [object] $stgCtx)
@@ -28,21 +15,7 @@ static [void] UploadStorageBlobContent([string] $fileName, [string] $blobName, [
 
     static [object] GetStorageBlobContent([string] $fileName, [string] $blobName, [string] $containerName, [object] $stgCtx)
 	{
-        if([FeatureFlightingManager]::GetFeatureStatus("IsSetAzStorageBlobAvailable","*") -eq $true)
-        {
-            $result = Get-AzStorageBlobContent -Blob $blobName -Container $containerName -Destination $fileName -Context $stgCtx -Force 
-        }
-        else {
-            $blob = Get-AzStorageBlob -Container $containerName -Blob $blobName -Context $stgCtx
-            $task = $blob.ICloudBlob.DownloadToFileAsync($fileName,[System.IO.FileMode]::Create)
-            $task.Wait()
-            if (-not ($task.IsCompleted -and !$task.IsFaulted))
-            {
-				#Need to change write method
-				Write-Debug "Downloading file from" + $blobName + " has failed!!"
-            }
-            $result = $blob 
-        }
+        $result = Get-AzStorageBlobContent -Blob $blobName -Container $containerName -Destination $fileName -Context $stgCtx -Force 
         return $result
     }
 }
