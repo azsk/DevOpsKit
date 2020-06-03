72659eb829ae563c5343605546a6c60f88c7d1c9
Manish Prabhu <mprabhu@microsoft.com>
2020-06-03 16:35:24 +0530

    Trial.

--- src/AzSK.AzureDevOps/SVT/SVT.ps1
+++ src/AzSK.AzureDevOps/SVT/SVT.ps1
@@ -24,7 +24,7 @@ function Get-AzSKAzureDevOpsSecurityStatus
 	   Agent name for which the security evaluation has to be performed.	
 
 	.NOTES
-	This command helps the application team to verify whether their Azure resources are compliant with the security guidance or not 
+	This command helps the application team to verify whether their Azure resources are compliant with the security guidance or not. 
 
 	.LINK
 	https://aka.ms/azskossdocs 
