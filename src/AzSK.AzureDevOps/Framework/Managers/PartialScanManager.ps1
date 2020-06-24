a749827ee58f49ba79ccbc4f507aca9a5f4e5b45
Garima Singh <v-garisi@microsoft.com>
2020-06-24 15:44:25 +0530

    UPC Extension changes

--- src/AzSK.AzureDevOps/Framework/Managers/PartialScanManager.ps1
+++ src/AzSK.AzureDevOps/Framework/Managers/PartialScanManager.ps1
@@ -9,7 +9,8 @@ class PartialScanManager
 	[PSObject] $ControlSettings;
 	hidden [ActiveStatus] $ActiveStatus = [ActiveStatus]::NotStarted;
     hidden [string] $AzSKTempStatePath = (Join-Path $([Constants]::AzSKAppFolderPath) "TempState" | Join-Path -ChildPath "PartialScanData");
-    hidden [bool] $StoreResTrackerLocally = $false;
+	hidden [bool] $StoreResTrackerLocally = $false;
+	hidden [string] $scanSource = $null;
 
 
 	hidden static [PartialScanManager] $Instance = $null;
@@ -66,13 +67,46 @@ class PartialScanManager
 
      hidden [void] GetResourceTrackerFile($subId)
     {
+		$this.scanSource = [AzSKSettings]::GetInstance().GetScanSource();
+         write-host $this.scanSource
 		#Validating the configuration of storing resource tracker file
         if($null -ne $this.ControlSettings.PartialScan)
 		{
 			$this.StoreResTrackerLocally = [Bool]::Parse($this.ControlSettings.PartialScan.StoreResourceTrackerLocally);
-        }
+		}
+		If ($this.scanSource -eq "CICD") # use extension storage in case of CICD partial scan
+		{
+			$this.subId = $subId
+            if($null -eq $this.ScanPendingForResources)
+		    {
+				if(![string]::isnullorwhitespace($this.subId))
+				{
+					$rmContext = [ContextHelper]::GetCurrentContext();
+					$user = "";
+					$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$rmContext.AccessToken)))
+					$uri = [Constants]::TestStorageUri -f $this.subId, $this.subId, "ResourceTrackerFile"
+					try {
+						$webRequestResult = Invoke-RestMethod -Uri $uri -Method Get -ContentType "application/json" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+						$this.ScanPendingForResources = $webRequestResult.value | ConvertFrom-Json
+						write-host "Get request ran successfully"
+					}
+					catch
+					{
+						write-host "error in Get request from extension storage :  $uri"
+						# $body = @{"id" = "ResourceTrackerFile"; "Project" = $this.subId; } | ConvertTo-Json
+						# $uri = [Constants]::TestStorageUri -f $this.subId, $this.subId, "ResourceTrackerFile"  
+						# try {
+						# 	$webRequestResult = Invoke-WebRequest -Uri $uri -Method Put -ContentType "application/json" -Headers @{Authorization = ("Basic {0}" -f $base64AuthInfo) } -Body $body	
+						# }
+						# catch {	
+						# 	Write-Host "Error: Could not configure host storage for resource tracker file of org-specific controls" -ForegroundColor Red
+						# }
+					}		
+			    }
+			}
+		}
         #Use local Resource Tracker files for partial scanning
-        if ($this.StoreResTrackerLocally) 
+        elseif ($this.StoreResTrackerLocally) 
         {
             $this.subId = $subId
             if($null -eq $this.ScanPendingForResources)
@@ -153,8 +187,31 @@ class PartialScanManager
 	# Method to remove obsolete Resource Tracker file
 	[void] RemovePartialScanData()
 	{
+		If ($this.scanSource -eq "CICD")
+		{
+            if($null -ne $this.ResourceScanTrackerObj)
+		    {
+				$rmContext = [ContextHelper]::GetCurrentContext();
+				$user = "";
+				$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$rmContext.AccessToken)))
+				$uri = [Constants]::TestStorageUri -f $this.subId, $this.subId, "ResourceTrackerFile"  
+
+				try {
+					if ($this.ScanPendingForResources -ne $null){
+						$webRequestResult = Invoke-WebRequest -Uri $uri -Method Delete -ContentType "application/json" -Headers @{Authorization = ("Basic {0}" -f $base64AuthInfo) } 
+						$this.ResourceScanTrackerObj = $null
+						write-host "Resource tracker removed successfully"
+
+					}
+				}
+				catch {
+					write-host "error in delete request from extension storage :  $uri"
+				}
+			}
+		}
+
         #Use local Resource Tracker files for partial scanning
-        if ($this.StoreResTrackerLocally) 
+        elseif ($this.StoreResTrackerLocally) 
             {
 		    if($null -ne $this.ResourceScanTrackerObj)
 		    {
@@ -213,7 +270,34 @@ class PartialScanManager
 
 	[void] WriteToResourceTrackerFile()
 	{
-        if ($this.StoreResTrackerLocally) 
+		If ($this.scanSource -eq "CICD")
+		{
+            if($null -ne $this.ResourceScanTrackerObj)
+		    {
+				if(![string]::isnullorwhitespace($this.subId))
+				{
+					$rmContext = [ContextHelper]::GetCurrentContext();
+					$user = "";
+					$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$rmContext.AccessToken)))
+					$scanObject = $this.ResourceScanTrackerObj | ConvertTo-Json
+					$body = @{"id" = "ResourceTrackerFile"; "value"= $scanObject;} | ConvertTo-Json
+					$uri = [Constants]::TestStorageUri -f $this.subId, $this.subId, "ResourceTrackerFile"  
+
+					try {
+						if ($this.ScanPendingForResources -ne $null){
+							$webRequestResult = Invoke-WebRequest -Uri $uri -Method Delete -ContentType "application/json" -Headers @{Authorization = ("Basic {0}" -f $base64AuthInfo) }
+						}
+						$webRequestResult = Invoke-WebRequest -Uri $uri -Method Put -ContentType "application/json" -Headers @{Authorization = ("Basic {0}" -f $base64AuthInfo) } -Body $body 
+						write-host "Resource tracker updated successfully"
+					}
+					catch
+					{
+						write-host "error in WriteToResourceTrackerFile method : $uri"
+					}		
+			    }
+			}
+		}
+        elseif ($this.StoreResTrackerLocally) 
         {
 		        if($null -ne $this.ResourceScanTrackerObj)
 		        {
@@ -253,8 +337,14 @@ class PartialScanManager
 			{
 				return;
 			}
-			
-            if ($this.StoreResTrackerLocally) 
+			If ($this.scanSource -eq "CICD") # use extension storage in case of CICD partial scan
+			{
+				if(![string]::isnullorwhitespace($this.ScanPendingForResources))
+				{
+					$this.ResourceScanTrackerObj = $this.ScanPendingForResources	
+				}
+			}
+            elseif ($this.StoreResTrackerLocally) 
             {
 			    if(![string]::isnullorwhitespace($this.subId)){
 				    if(-not (Test-Path (Join-Path $this.AzSKTempStatePath $this.subId)))
