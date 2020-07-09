e48783a11e87479413fd3f97938ee76f336d9268
Rohit Yadav <v-royad@microsoft.com>
2020-07-09 21:22:51 +0530

    Adding Suggested changes

--- src/AzSK/Framework/Core/AzSKInfo/SPNInfo.ps1
+++ src/AzSK/Framework/Core/AzSKInfo/SPNInfo.ps1
@@ -3,38 +3,45 @@ Set-StrictMode -Version Latest
 
 class SPNInfo: CommandBase
 {    
-	hidden $validatedUri = [string]::Empty
+	hidden $GraphOwnedObjectsAPIUri = [string]::Empty
 	SPNInfo([string] $subscriptionId, [InvocationInfo] $invocationContext): 
         Base($subscriptionId, $invocationContext) 
     { 
 		$this.DoNotOpenOutputFolder = $true;
-		$this.validatedUri = 'https://graph.microsoft.com/v1.0/me/ownedObjects';
+		$this.GraphOwnedObjectsAPIUri = 'https://graph.microsoft.com/v1.0/me/ownedObjects';
 	}
 	
 	GetSPNInfo()
 	{
-        $rmContext = [ContextHelper]::GetCurrentRMContext();
+		$ownedSPNs = @()
+		$usedSPNs = @()
+		$notInUsedSPNs = @()
+		$rmContext = [ContextHelper]::GetCurrentRMContext();
         $this.PublishCustomMessage([Constants]::DoubleDashLine + "`r`nFetching SPN(s) Details...`r`n" + [Constants]::DoubleDashLine);
-		#$this.PublishCustomMessage("`r`nSPNs Info`r`n" + [Constants]::SingleDashLine, [MessageType]::Default)
 		#Get all owned SPNs
         $ownedSPNDetails = $this.GetOwnedSPNList();
 		#Get SPNs start with AzSK_CA
-        $ownedSPNs = $this.FilterOwnedSPNStartWithAzSK($ownedSPNDetails);
+		if($null -ne $ownedSPNDetails)
+		{
+			#Filter OwnedSPN start with AzSK_CA
+			$ownedSPNs = $ownedSPNDetails | Where-Object { $_.displayName -like "AzSK_CA*"}
+		}
 		
-        if($ownedSPNs -ne $null -and ($ownedSPNs | Measure-Object).Count -ne 0)
+		if($null -ne $ownedSPNs -and ($ownedSPNs | Measure-Object).Count -ne 0)
         {
 			#Add subscriptionName and subscriptionId in ownedSPNs list
 			$ownedSPNsObject = $this.CreateOwnedSPNObject($ownedSPNs.appId,$rmContext)
 			#Get SPNsResponse which contain list of used SPNs
             $SPNsResponse = [RemoteApiHelper]::FetchUsedSPNList($ownedSPNsObject);
 		    #Convert SPNsResponse into usedSPNs, which contain list of CA used SPNs 
-		    $usedSPNs = $this.GetUsedSPNs($SPNsResponse);
-		    #Get list of notInUsedSPNs, which contain list of not in CA used SPNs
-			
-			#If usedSPN count is 0 then no need to call GetUnusedSPNs()
-			if(($usedSPNs | Measure-Object).Count -ne 0 -and $usedSPNs.appId -ne $null)
+		    if($null -ne $SPNsResponse)
 			{
-				$notInUsedSPNs = $this.GetUnusedSPNs($usedSPNs,$ownedSPNs);
+				$SPNsResponse | ConvertFrom-Json | where-object { $usedSPNs += $_ }
+			}
+			#Get list of notInUsedSPNs, which contain list of not in CA used SPNs
+			if(($null -ne $usedSPNs) -and ($usedSPNs | Measure-Object).Count -ne 0)
+			{
+				$notInUsedSPNs = $ownedSPNs | where-object { $_.appId -notin $usedSPNs.appId }
 			}
 			else 
 			{
@@ -74,7 +81,7 @@ class SPNInfo: CommandBase
 			$accessToken = [ContextHelper]::GetAccessToken($ResourceAppIdURI)
 			$header = "Bearer " + $accessToken
 			$headers = @{"Authorization"=$header;"Content-Type"="application/json";}
-			$uri=$this.validatedUri;
+			$uri=$this.GraphOwnedObjectsAPIUri;
 			$result = [WebRequestHelper]::InvokeGetWebRequest($uri, $headers); 
 			$ownedSPNDetails = $result | Select-Object -Property displayName,appId -Unique 
     		return ($ownedSPNDetails);
@@ -87,61 +94,12 @@ class SPNInfo: CommandBase
         }
 	} 
 
-	[PSObject] GetUsedSPNs($SPNsResponseObject)
-	{
-		$usedSPNs = @();
-		if($null -ne  $SPNsResponseObject)
-		{
-			$parsedSPNsList = $SPNsResponseObject | convertfrom-json;
-			foreach ($item in $parsedSPNsList) 
-			{
-				$usedSPNs += $item;
-			}
-		}
-		return ($usedSPNs);
-	}
-
-
-	[PSObject] FilterOwnedSPNStartWithAzSK($ownedSPNDetails)
-	{
-		$ownedSPNUsedInCA = @();
-		if($null -ne  $ownedSPNDetails)
-		{
-			foreach ($ownedSPN in $ownedSPNDetails) 
-			{
-				if($ownedSPN.displayName -like 'AzSK_CA*')
-				{
-					$ownedSPNUsedInCA += $ownedSPN;
-				}
-			}
-		}
-		return ($ownedSPNUsedInCA);
-	} 
-
-	
-	[PSObject] GetUnusedSPNs($usedSPNs,$ownedSPNs)
-	{
-		$notInUsedSPNs = @();
-		if($null -ne $ownedSPNs)
-		{
-			foreach ($SPNId in $ownedSPNs) 
-			{
-				if($SPNId.appId -notin $usedSPNs.appId)
-				{
-					$notInUsedSPNs += $SPNId;
-				}
-				
-			}
-		}
-		return ($notInUsedSPNs);
-	}
-
 	[PSObject] CreateOwnedSPNObject($ownedSPNApplicationId,$rmcontext)
 	{
-		$ownedSPNObject = "" | Select-Object subscriptionId, subscriptionName, userSPNAppId
-		$ownedSPNObject.subscriptionId = $rmcontext.Subscription.Id
-		$ownedSPNObject.subscriptionName = $rmcontext.Subscription.Name
-		$ownedSPNObject.userSPNAppId = $ownedSPNApplicationId
+		$ownedSPNObject = "" | Select-Object SubscriptionId, SubscriptionName, UserSPNAppIds
+		$ownedSPNObject.SubscriptionId = $rmcontext.Subscription.Id
+		$ownedSPNObject.SubscriptionName = $rmcontext.Subscription.Name
+		$ownedSPNObject.UserSPNAppIds = $ownedSPNApplicationId
 		return ($ownedSPNObject);
 	}
 
