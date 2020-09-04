7f400566d6fa670143972dff8f4d14cb911fffe3
Sourabh Kumrawat <63220979+v-soukum@users.noreply.github.com>
2020-09-04 13:30:48 +0530

    pr comment resolved

--- src/AzSK/Framework/Core/ARMChecker/ARMCheckerStatus.ps1
+++ src/AzSK/Framework/Core/ARMChecker/ARMCheckerStatus.ps1
@@ -375,7 +375,7 @@ class ARMCheckerStatus: EventBase
 			}	
 		}
 		
-		if($csvResults.Count -ge 0)
+		if($csvResults.Count -gt 0)
 		{
 		    $csvResults| Export-Csv $csvFilePath -NoTypeInformation -Force
         }
