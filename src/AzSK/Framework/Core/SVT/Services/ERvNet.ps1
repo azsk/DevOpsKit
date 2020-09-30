dda540e02087098feee7e36c0f111b67bbdbff43
Sourabh Kumrawat <63220979+v-soukum@users.noreply.github.com>
2020-09-30 13:54:03 +0530

    PR comment resolved

--- src/AzSK/Framework/Core/SVT/Services/ERvNet.ps1
+++ src/AzSK/Framework/Core/SVT/Services/ERvNet.ps1
@@ -425,7 +425,7 @@ class ERvNet : SVTIaasBase
 
         if($null -ne $resources)
         {
-            #control will pass for "Microsoft.EventGrid" type resources
+            #control will exempt "Microsoft.EventGrid" type resources in the RG
             $nonApprovedResources = [array]($resources | Where-Object { -not ($_.ResourceType.StartsWith("Microsoft.Network","CurrentCultureIgnoreCase") -or $_.ResourceType.StartsWith("Microsoft.EventGrid","CurrentCultureIgnoreCase"))})            
             if($null -ne $nonApprovedResources )
             {
